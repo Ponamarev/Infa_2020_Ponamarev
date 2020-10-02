0a340fe1d04aef0638176eb410abab5a565886f7
Ponamarev <ponamarev.ev@phystech.edu>
2020-10-02 11:56:34 +0300

    добавил пересчет размеров

--- Lab4/Code/Rabbit_Norm_2020.py
+++ Lab4/Code/Rabbit_Norm_2020.py
@@ -19,33 +19,43 @@ def settings(FPS):
             if event.type == pygame.QUIT:
                 finished = True
 
-
+# зададим часто используемые цвета заранее
 grey = (128, 128, 128)
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+def eye(x, y, alphaX, alphaY): # FIX
+    ellipse(screen, (grey, alphaX * 400, alphaY * 200, alphaX * 200, alphaY * 200), 0)
 
 
-def rabbit_head(x, y, width, height):
+def rabbit_head(x, y, alphaX, alphaY):  # FIX
     """
     рисует голову.
     """
-    ellipse(screen, (grey, width, height, 100, 200), 0)
-    pass
+    #
+    ellipse(screen, (grey, alphaX * 400, alphaY * 200,  alphaX * 200, alphaY * 200), 0)
+
+    #
 
 
-def rabbit_body(x, y, width, height):
+
+
+
+def rabbit_body(x, y, alphaX, alphaY): # FIX
     """
 
     """
     pass
 
 
-def rabbit_legs(x, y, width, height):
+def rabbit_legs(x, y, alphaX, alphaY):  # FIX
     """
 
     """
     pass
 
 
-def rabbit_somethings(x, y, width, height):
+def rabbit_somethings(x, y, alphaX, alphaY):  # FIX
     """
 
 
@@ -53,9 +63,10 @@ def rabbit_somethings(x, y, width, height):
     pass
 
 
-def rabbit(x, y, width, height):
+def rabbit(x, y, alphaX, alphaY):
     """
     Рисует кролика.
+
     x, y - координаты левой нижней точки прямоугольника,
     в который вписан кролик.
     width - ширина прямоугольника.
@@ -63,13 +74,18 @@ def rabbit(x, y, width, height):
     """
     screen = pygame.display.set_mode((width, height))
 
-    rabbit_head(x, y, width, height)
-    rabbit_body(x, y, width, height)
-    rabbit_legs(x, y, width, height)
-    rabbit_somethings(x, y, width, height)
+    # Коэфиценты для расширения линейных размеров по соотв. осям.
+    # Они нужны, т к у меня есть возможность самому выбрать размер окна.
+    alphaX = width / 1000
+    alphaY = height / 2000
+
+    rabbit_head(x, y, alphaX, alphaY)
+    rabbit_body(x, y, alphaX, alphaY)
+    rabbit_legs(x, y, alphaX, alphaY)
+    rabbit_somethings(x, y, alphaX, alphaY)
 
     # Подключаем настройки pygame.
     settings(30)
 
 
-rabbit(2, 434, 334, 434)
+rabbit(2, 434, 1000, 2000)
