317c4201599a0b47081f90af34a8359bc8cdd301
Ponamarev Evgeny <ponamarev.ev@phystech.edu>
2020-12-06 19:03:55 +0300

    Refactored

--- Physic_simulation/Physic arm_gun.py
+++ Physic_simulation/Physic arm_gun.py
@@ -26,7 +26,7 @@ class Point_of_gun():
         """Перемещает точку"""
 
         self.V += self.a * dt
-        self.L += self.V * dt
+        self.L -= self.V * dt
         # Нулевая точка должна быть закреплена.
 
     def count_force(self, obj1, obj2):
@@ -38,20 +38,23 @@ class Point_of_gun():
         """
         global k
         L = 0.1 / count_of_points  # м.
-        self.T1 = obj1.T2
 
-        if k * (L - (self.L - obj1.L)) * count_of_points > 0:
-            T1 = k * (L - (self.L - obj1.L)) * count_of_points
+        if self.L - obj1.L > L:  # Нужно занести в общую функцию под NUMBA!.
+            T1 = k * count_of_points * ((self.L - obj1.L) - L)
+        elif obj1.L - self.L > L:
+            T1 = k * count_of_points * ((self.L - obj1.L) + L)
         else:
             T1 = 0
 
-        if k * (obj2.L - self.L - L) * count_of_points > 0:
-            T2 = k * (obj2.L - self.L - L) * count_of_points
+        if obj2.L - self.L > L:  # Нужно занести в общую функцию под NUMBA!.
+            T2 = k * count_of_points * ((obj2.L - self.L) - L)
+        elif self.L - obj2.L > L:
+            T2 = k * count_of_points * ((obj2.L - self.L) + L)
         else:
             T2 = 0
 
-        self.a = (T2 - T1) / self.m
-
+        self.a = (T1 - T2) / self.m
+        self.T1 = T1
         self.T2 = T2
 
         # Нужно обработать первую и последнюю точки.
@@ -63,8 +66,10 @@ class Point_of_gun():
         :return: None
         """
         L = 0.1 / count_of_points  # м.
-        if k * (massive[1].L - L) > 0:
+        if massive[1].L - L > 0:
             self.T2 = k * (massive[1].L - L) * count_of_points
+        elif L + massive[1].L < 0:
+            self.T2 = k * (massive[1].L + L) * count_of_points
         else:
             self.T2 = 0
 
@@ -74,7 +79,6 @@ class Point_of_gun():
         :return:  None
         """
         pygame.draw.circle(screen, (193, 193, 0), (int(550 - self.L * 2500), 223), 3, 0)
-
 
 
 class Shell():
@@ -87,13 +91,7 @@ class Shell():
 
     def move_shell(self, dt):
         """Передвигает снаряд"""
-        # if (left_points[count_of_points].T2 + right_points[count_of_points].T2) < 0:
-        if 2 * right_points[count_of_points - 1].T2 < 0:
-            self.a = 0
-        else:
-            # self.a = (left_points[count_of_points].T2 + right_points[count_of_points].T2) / self.M
-            self.a = 2 * right_points[count_of_points - 1].T2 / self.M
-
+        self.a = 2 * right_points[count_of_points - 1].T2 / self.M
         self.V += self.a * dt
         self.L -= self.V * dt
 
@@ -105,13 +103,13 @@ class Shell():
         pygame.draw.circle(screen, (193, 0, 0), (int(550 - self.L * 2500), 223), 3, 0)
 
 
-count_of_points = 100000
-delta_time = 1 / 10000000  # с.
+count_of_points = 10
+delta_time = 1 / 100000000000000  # с.
 left_points = []
 right_points = []
 lenght_of_gun = 0.10  # м.
-weight_of_gun = 11.030  # кг.
-k = 10 / 0.2  # Н / м.
+weight_of_gun = 0.00000000000000000000030  # кг.
+k = 10 / 0.1  # Н / м.
 frame = 0
 finished = False
 
@@ -152,7 +150,7 @@ def main():
         print('скорость' + str(shell.V))
         print(right_points[count_of_points - 1].V)
 
-        if frame >= 10:
+        if frame >= 10000:
             frame = 0
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -174,5 +172,41 @@ def main():
     # Надо решить проблему со знаками в движении снаряда. и у частиц тоже.
 
 
+def test_first_force():
+    l = lenght_of_gun / count_of_points * 2
+    dm = weight_of_gun / count_of_points
+    # left_points.append(Point_of_gun(l, dm))
+    right_points.append(Point_of_gun(0, dm))
+    right_points.append(Point_of_gun(-l, dm))
+    right_points[0].force_first_point(right_points)
+    print(right_points[0].T2)
+
+
+def test_count_force():
+    l = lenght_of_gun / count_of_points * 2
+    dm = weight_of_gun / count_of_points
+    # left_points.append(Point_of_gun(l, dm))
+    right_points.append(Point_of_gun(0, dm))
+    right_points.append(Point_of_gun(l, dm))
+    right_points.append(Point_of_gun(2*l, dm))
+    right_points[1].count_force(right_points[0], right_points[2])
+    print(right_points[1].T1)
+    print(right_points[1].T2)
+    print(right_points[1].a)
+
+
+def test_mover():
+    l = lenght_of_gun / count_of_points * 2
+    dm = weight_of_gun / count_of_points
+    # left_points.append(Point_of_gun(l, dm))
+    right_points.append(Point_of_gun(0, dm))
+    right_points[0].a = 1
+    right_points[0].move_points(1)
+    print(right_points[0].L)
+
+
 if __name__ == '__main__':
     main()
+    # test_first_force()
+    # test_count_force()
+    # test_mover()
